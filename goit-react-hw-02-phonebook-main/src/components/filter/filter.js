d3f84acf7a3a9bcacc75d3090b12433f625551f4
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-07-24 20:57:35 +0300

    24.07

---
+++ goit-react-hw-02-phonebook-main/src/components/filter/filter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import s from './Filter.module.css'
+
+const Filter =({value,onChange})=>(
+  <label className={s.label}><b>Фильтр по имени</b><input className={
+    s.input
+  } type="Text" value={value} onChange={onChange}/></label>
+
+);
+        
+
+export default Filter;
